169006cb80435629f22289c9c7d59090c79b3e39
perrowito <perrowito35@gmail.com>
2026-06-17 04:32:43 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,27 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, send_from_directory
 import os
 
 app = Flask(__name__)
 
-UPLOAD_FOLDER = "uploads"
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
+PHOTOS_FOLDER = "uploads/photos"
+VIDEOS_FOLDER = "uploads/videos"
+
+os.makedirs(PHOTOS_FOLDER, exist_ok=True)
+os.makedirs(VIDEOS_FOLDER, exist_ok=True)
 
 @app.route("/")
 def home():
     return "SyncGallery Backend OK"
-
 @app.route("/upload", methods=["POST"])
 def upload():
 
     if "photo" not in request.files:
-        return {"error": "No photo"}, 400
+        return {"error": "No file"}, 400
 
     file = request.files["photo"]
 
-    ruta = os.path.join(
-        UPLOAD_FOLDER,
-        file.filename
-    )
+    nombre = file.filename.lower()
+
+    if nombre.endswith(".mp4"):
+
+        ruta = os.path.join(
+            VIDEOS_FOLDER,
+            file.filename
+        )
+
+    else:
+
+        ruta = os.path.join(
+            PHOTOS_FOLDER,
+            file.filename
+        )
 
     file.save(ruta)
 
@@ -29,25 +42,51 @@ def upload():
         "ok": True,
         "archivo": file.filename
     }
+
 @app.route("/clear")
 def clear():
 
-    for archivo in os.listdir(UPLOAD_FOLDER):
+    for carpeta in [
+        PHOTOS_FOLDER,
+        VIDEOS_FOLDER
+    ]:
 
-        ruta = os.path.join(
-            UPLOAD_FOLDER,
-            archivo
-        )
+        for archivo in os.listdir(carpeta):
 
-        os.remove(ruta)
+            ruta = os.path.join(
+                carpeta,
+                archivo
+            )
+
+            os.remove(ruta)
 
     return {"ok": True}
+
 @app.route("/files")
 def files():
 
     return {
-        "files": os.listdir(UPLOAD_FOLDER)
+        "photos": os.listdir(
+            PHOTOS_FOLDER
+        ),
+        "videos": os.listdir(
+            VIDEOS_FOLDER
+        )
     }
+@app.route("/photo/<filename>")
+def photo(filename):
+
+    return send_from_directory(
+        PHOTOS_FOLDER,
+        filename
+    )
+@app.route("/video/<filename>")
+def video(filename):
+
+    return send_from_directory(
+        VIDEOS_FOLDER,
+        filename
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
