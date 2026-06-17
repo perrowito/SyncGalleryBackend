1044d22d03b05cfb1e88787724e2c97dc44a19d2
perrowito <perrowito35@gmail.com>
2026-06-16 23:41:23 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask
+from flask import Flask, request
+import os
 
 app = Flask(__name__)
+
+UPLOAD_FOLDER = "uploads"
+os.makedirs(UPLOAD_FOLDER, exist_ok=True)
 
 @app.route("/")
 def home():
     return "SyncGallery Backend OK"
+
+@app.route("/upload", methods=["POST"])
+def upload():
+
+    if "photo" not in request.files:
+        return {"error": "no photo"}, 400
+
+    file = request.files["photo"]
+
+    ruta = os.path.join(
+        UPLOAD_FOLDER,
+        file.filename
+    )
+
+    file.save(ruta)
+
+    return {"ok": True}
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
